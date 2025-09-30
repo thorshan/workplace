6747014e0ddaa6e331edbb4dfa9631cffb288a11
unknown <myothunaing18@gmail.com>
2025-09-30 14:21:21 +0630

    FIX_ERROR_EDIT-USER

--- controllers/authController.js
+++ controllers/authController.js
@@ -110,7 +110,7 @@ const updateUser = async (req, res) => {
         const { name, email, password, role } = req.body;
 
         // Check the user
-        const user = await User.findById({id});
+        const user = await User.findById(id);
         if(!user) 
             return res.status(404).json({ message: "User not found" });
 

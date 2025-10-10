4925445d86c9ef4e66559afbfb62bf2825855dcd
unknown <myothunaing18@gmail.com>
2025-10-10 11:06:05 +0630

    FIX_LEAVE_ROUTE

--- routes/empLeave.js
+++ routes/empLeave.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const empLeaveController = require("../controllers/empLeaveController");
 
 // Employee routes
-router.post("/leaves", empLeaveController.getEmployeeLeaves);
+router.get("/leaves/:id", empLeaveController.getEmployeeLeaves);
 router.post("/leaves/create", empLeaveController.createEmployeeLeave);
 router.delete("/leaves/:id", empLeaveController.cancelEmployeeLeave);
 
